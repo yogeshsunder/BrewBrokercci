4ba42b5add6ac50ff134b9b2193da383062b2fa9
yogeshsunder <sunderyogesh@gmail.com>
2019-03-18 17:52:16 +0530

    Making changes in pom.xml

--- src/test/java/BrewBrokercircleci/BrewBrokercircleci/OpenBrowser.java
+++ src/test/java/BrewBrokercircleci/BrewBrokercircleci/OpenBrowser.java
@@ -2,6 +2,7 @@ package BrewBrokercircleci.BrewBrokercircleci;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class OpenBrowser {
@@ -12,7 +13,14 @@ public class OpenBrowser {
 	public void openBrowser()
 	{
 		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
+		options.addArguments("window-size=1400,800");
+	
+		driver = (WebDriver) new ChromeDriver(options);
+		//driver=(WebDriver) new ChromeDriver();
+		
+		
 		driver.get("https://brewbroker.com/");
 	}
 
